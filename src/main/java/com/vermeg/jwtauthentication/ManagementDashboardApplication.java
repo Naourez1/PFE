67bed85733a8172d2b20d6540ff06f011e6f0ff4
Naourez <nnasr65@gmail.com>
2019-04-14 22:45:41 +0200

    third commit

--- src/main/java/com/vermeg/jwtauthentication/ManagementDashboardApplication.java
+++ src/main/java/com/vermeg/jwtauthentication/ManagementDashboardApplication.java
@@ -1,12 +1,17 @@
 package com.vermeg.jwtauthentication;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import com.vermeg.jwtauthentication.model.User;
 import com.vermeg.jwtauthentication.repository.UserRepository;
-
+@Configuration
+@EnableAutoConfiguration
+@ComponentScan({ "com.example.demo", "controller", "service" })
 @SpringBootApplication
 public class ManagementDashboardApplication implements CommandLineRunner{
 	
--- src/main/java/com/vermeg/jwtauthentication/security/WebSecurityConfig.java
+++ src/main/java/com/vermeg/jwtauthentication/security/WebSecurityConfig.java
@@ -58,7 +58,6 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
         http.cors().and().csrf().disable().
                 authorizeRequests()
                 .antMatchers("/api/auth/**").permitAll()
-                .anyRequest().authenticated()
                 .and()
                 .exceptionHandling().authenticationEntryPoint(unauthorizedHandler).and()
                 .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
